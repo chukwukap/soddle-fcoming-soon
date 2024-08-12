13e6beb687ca269eb6f4534dce8f04dbdd0a7f38
Chukwuka Uba <promiseuba67@gmail.com>
2024-08-13 00:06:43 +0100

    update tweets games

---
+++ web/app/games/emojis-game/page.tsx
@@ -0,0 +1,3 @@
+export default function EmojisGame() {
+  return <div>EmojisGame</div>;
+}
--- web/app/games/tweets-game/_components/game.tsx
+++ web/app/games/tweets-game/_components/game.tsx
@@ -7,6 +7,8 @@ import UserInput from '@/components/ui/input';
 import Button from '@/components/ui/button2';
 import Beveled from '@/components/ui/trapezoid';
 import BeveledRectangle from '@/components/ui/beveled-rectangle';
+import Container from '@/components/layout/container';
+import Image from 'next/image';
 
 interface Tweet {
   content: string;
@@ -61,54 +63,77 @@ const TweetsGame: React.FC<TweetsGameProps> = ({ kols, onGameComplete }) => {
   //   if (!currentTweet) return <div className="text-white">Loading...</div>;
 
   return (
-    <BeveledRectangle className="  flex flex-col gap-5">
-      <TimerDisplay />
-      <ChallengeWrapper className="grid">
-        <h2 className="text-2xl font-bold mb-4 text-center">
-          Who published that tweet?
-        </h2>
-        <div className="bg-[#181716] p-2 w-full ">
-          Caitlyn Jenner building on Base, and following me isn't what I
-          expected to see on my bingo card this month.
-        </div>
-      </ChallengeWrapper>
+    <div className="flex flex-col gap-5 flex-grow">
+      <Container className="flex items-center justify-center">
+        <TimerDisplay />
+      </Container>
+      <Container>
+        <ChallengeWrapper className="grid">
+          <h2 className="text-2xl font-bold mb-4 text-center">
+            Who published that tweet?
+          </h2>
+          <div className="bg-[#181716] p-2 w-full text-xl">
+            Caitlyn Jenner building on Base, and following me isn't what I
+            expected to see on my bingo card this month.
+          </div>
+        </ChallengeWrapper>
+      </Container>
 
-      <div className="mb-6">
-        <Button
-          onClick={() => console.log('Button clicked')}
-          className="w-5 "
-        ></Button>{' '}
+      <Container>
         <UserInput
-          placeholder="Enter text"
+          placeholder="Enter a personality..."
           onChange={(e) => console.log(e.target.value)}
+          className="bg-[#181716] placeholder-slate-600 "
         />{' '}
-      </div>
-
-      <div className="space-y-4">
-        {options.map((option, index) => (
-          <button
-            key={index}
-            // onClick={() => handleGuess(option)}
-            disabled={gameOver}
-            className={`w-full p-4 rounded-md text-white text-lg font-semibold flex items-center justify-center transition-colors ${
-              index % 2 === 0
-                ? 'bg-green-600 hover:bg-green-700'
-                : 'bg-red-600 hover:bg-red-700'
-            } disabled:opacity-50 disabled:cursor-not-allowed`}
-          >
-            <span className="mr-2">👤</span> {option}
-          </button>
-        ))}
-      </div>
-
-      {gameOver && (
-        <div className="mt-6 text-center">
-          <h3 className="text-xl font-bold">
-            Correct! You guessed in {guesses} attempts.
-          </h3>
+      </Container>
+      <Container>
+        <div className="space-y-4">
+          {options.map((option, index) => (
+            <button
+              key={index}
+              // onClick={() => handleGuess(option)}
+              disabled={gameOver}
+              className={`w-full p-4 rounded-md text-white text-lg font-semibold flex items-center justify-center transition-colors ${
+                index % 2 === 0
+                  ? 'bg-green-600 hover:bg-green-700'
+                  : 'bg-red-600 hover:bg-red-700'
+              } disabled:opacity-50 disabled:cursor-not-allowed`}
+            >
+              <span className="mr-2">👤</span> {option}
+            </button>
+          ))}
         </div>
-      )}
-    </BeveledRectangle>
+      </Container>
+      <Container className="flex gap-2 flex-col">
+        <NameTag name="Vitalik Buterin" isCorrect={true} />
+        <NameTag name="Patrick Collins" isCorrect={false} />
+      </Container>
+    </div>
+  );
+};
+
+interface NameTagProps {
+  name: string;
+  isCorrect: boolean;
+}
+
+const NameTag: React.FC<NameTagProps> = ({ name, isCorrect }) => {
+  return (
+    <div
+      className={` ${
+        isCorrect ? 'bg-green-500' : 'bg-[#D21210]'
+      } text-white py-5 px-4  flex items-center justify-center space-x-2`}
+    >
+      <div className="relative w-7 h-7">
+        <Image
+          src="/images/default-avatar.png"
+          alt="Personality icon"
+          layout="fill"
+          className="rounded-full"
+        />
+      </div>
+      <span className="font-semibold">{name}</span>
+    </div>
   );
 };
 
