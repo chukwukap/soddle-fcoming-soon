eacaffb3260c2b20526bd2987f94bb5a3eb0c37e
chukwuka uba <promiseuba67@gmail.com>
2024-10-01 05:44:22 -0700

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,4 +1,5 @@
 import './global.css';
+import Image from 'next/image';
 import { MainLayout } from '@/components/layout/main';
 import Providers from '@/components/providers';
 import { satoshi } from '@/lib/fonts';
@@ -15,17 +16,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${satoshi.variable} font-satoshi`}>
-      <body
-        className={`
-          bg-[#181716] 
-          bg-[url('/backgrounds/riddle_2_background.png')] 
-          bg-cover 
-          bg-center 
-          bg-fixed 
-          bg-no-repeat
-        `}
-      >
-        {' '}
+      <body className="bg-[#181716] relative min-h-screen">
+        <Image
+          src="/backgrounds/riddle_2_background.png"
+          alt="Background"
+          fill
+          priority
+          quality={100}
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'center',
+            zIndex: -1,
+          }}
+          unoptimized
+        />
         <Providers>
           <MainLayout>{children}</MainLayout>
         </Providers>
