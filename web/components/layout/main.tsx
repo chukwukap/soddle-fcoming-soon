6c2741b440ca6739377e9bfdf7de1b0359ce5ca7
chukwuka uba <promiseuba67@gmail.com>
2024-10-01 06:40:23 -0700

    Update main.tsx
    I think i just fixed the problem

--- web/components/layout/main.tsx
+++ web/components/layout/main.tsx
@@ -8,14 +8,7 @@ import Header from './header';
 export function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex flex-col  font-wremena min-h-screen">
-      <Image
-        src="/backgrounds/background_darkened_2.png"
-        alt="Background"
-        fill
-        priority
-        quality={100}
-        className="object-cover object-center"
-      />
+      
       <div className="relative z-10 flex flex-col justify-between min-h-screen py-4">
         <Header />
         <main className="flex-grow">{children}</main>
